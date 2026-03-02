09d4026dc1cd295d309e30ab5086cfdba6bdfd41
connerohnesorge <88785126+connerohnesorge@users.noreply.github.com>
2026-03-02 08:12:01 -0600

    Disable supermaven inline completion plugin (#16)

--- lua/plugins/supermaven-plugin.lua
+++ lua/plugins/supermaven-plugin.lua
@@ -1,12 +1,12 @@
 return {
-  "supermaven-inc/supermaven-nvim",
-  config = function()
-    require("supermaven-nvim").setup({
-      keymaps = {
-        accept_word = "<C-RIGHT>",
-        accept_suggestion = "<C-j>"
-      },
-      disable_inline_completion = false,
-    })
-  end,
+  -- "supermaven-inc/supermaven-nvim",
+  -- config = function()
+  --   require("supermaven-nvim").setup({
+  --     keymaps = {
+  --       accept_word = "<C-RIGHT>",
+  --       accept_suggestion = "<C-j>"
+  --     },
+  --     disable_inline_completion = false,
+  --   })
+  -- end,
 }
